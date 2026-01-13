fe9c7020b8f88f55663b84b6a901feb2f4a2a3b0
Elina Mazitova <elinamazitova14@gmail.com>
2026-01-13 19:20:42 +0300

    Add marks

--- tests/test_github.py
+++ tests/test_github.py
@@ -2,16 +2,14 @@ import pytest
 from pages.github_home_page import GithubHomePageDesktop, GithubHomePageMobile
 
 @pytest.mark.parametrize(
-    "page_class, browser, marker",
+    "page_class, browser",
     [
-        (GithubHomePageDesktop, (1920, 1080), "desktop"),
-        (GithubHomePageMobile, (375, 812), "mobile"),
+        pytest.param(GithubHomePageDesktop, (1920, 1080), marks=pytest.mark.desktop),
+        pytest.param(GithubHomePageMobile, (375, 812), marks=pytest.mark.mobile),
     ],
     indirect=["browser"]
 )
-def test_github_sign_in(page_class, browser, marker, request):
-    request.node.add_marker(getattr(pytest.mark, marker))
-
+def test_github_sign_in(page_class, browser):
     page = page_class(browser)
     page.open()
     page.click_sign_in()
