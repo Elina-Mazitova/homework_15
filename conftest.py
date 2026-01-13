7171b1706c64b2453feb481e3480eeb99332e3c6
Elina Mazitova <elinamazitova14@gmail.com>
2026-01-13 19:44:18 +0300

    Add hooks

--- conftest.py
+++ conftest.py
@@ -1,5 +1,48 @@
+import time
 import pytest
+import allure
 from selenium import webdriver
+
+def pytest_runtest_setup(item):
+    item.start_time = time.time()
+    print(f"\n=== START TEST: {item.name} ===")
+
+
+
+
+def pytest_collection_modifyitems(config, items):
+    for item in items:
+
+        if "mobile" in item.keywords:
+            item.user_properties.append(("device", "mobile"))
+
+        if "desktop" in item.keywords:
+            item.user_properties.append(("device", "desktop"))
+
+        if "slow" in item.keywords:
+            item.add_marker(pytest.mark.skip(reason="Skipping slow tests"))
+
+
+
+SLOW_THRESHOLD = 5.0  # секунды
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+
+    setattr(item, "rep_" + rep.when, rep)
+
+    if rep.when == "call":
+        duration = time.time() - item.start_time
+        item.duration = duration
+        print(f"=== TEST DURATION: {duration:.2f} seconds ===")
+
+        # если тест дольше 5 сек → считаем slow
+        if duration > SLOW_THRESHOLD:
+            item.add_marker(pytest.mark.slow)
+            print(f"=== TEST MARKED AS SLOW (duration {duration:.2f}s) ===")
+
 
 @pytest.fixture
 def browser(request):
@@ -10,5 +53,21 @@ def browser(request):
     driver.implicitly_wait(5)
 
     yield driver
+
+    if hasattr(request.node, "rep_call") and request.node.rep_call.failed:
+
+        with allure.step("Скриншот при падении"):
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name="screenshot",
+                attachment_type=allure.attachment_type.PNG
+            )
+
+        with allure.step("HTML страницы при падении"):
+            allure.attach(
+                driver.page_source,
+                name="page_source",
+                attachment_type=allure.attachment_type.HTML
+            )
 
     driver.quit()
