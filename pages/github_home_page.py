aae85b634e724ed55af19e86210f8824099024f2
Elina Mazitova <elinamazitova14@gmail.com>
2026-01-13 19:55:49 +0300

    divided classes to GithubHomePageMobile and GithubHomePageDesktop

--- pages/github_home_page.py
+++ pages/github_home_page.py
@@ -2,18 +2,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class GithubHomePageDesktop:
     SIGN_IN_BUTTON = (By.LINK_TEXT, "Sign in")
 
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def open(self):
         self.driver.get("https://github.com")
 
     def click_sign_in(self):
-        self.driver.find_element(*self.SIGN_IN_BUTTON).click()
-
+        self.wait.until(EC.element_to_be_clickable(self.SIGN_IN_BUTTON)).click()
 
 class GithubHomePageMobile:
     MENU_BUTTON = (By.CSS_SELECTOR, 'button[aria-label="Toggle navigation"], summary[aria-label="Toggle navigation"]')
@@ -32,3 +33,4 @@ class GithubHomePageMobile:
 
         sign_in = self.wait.until(EC.presence_of_element_located(self.SIGN_IN_LINK))
         self.driver.execute_script("arguments[0].click();", sign_in)
+
